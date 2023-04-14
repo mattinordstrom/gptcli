43c3585e74726489df7e888c3b75e2260b7a5ce0
Matti Nordström <matti.nordstrom@gmail.com>
2023-04-14 13:21:07 +0200

    Added dalle script

---
+++ dallecli.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import requests
+import argparse
+import json
+
+# Load API key from secret file
+with open("secret", "r") as f:
+    api_key = f.read().strip()
+
+# Set API endpoint and parameters
+api_url = "https://api.openai.com/v1/images/generations"
+size = "1024x1024"
+response_format = "url"
+
+# Parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("prompt", help="The prompt to generate an image from")
+
+args = parser.parse_args()
+prompt = args.prompt
+#prompt = "A red panda drinking coffee in a park"
+
+print(f"\033[90mSize: {size}\033[0m")
+print(f"\033[90mResponse format: {response_format}\033[0m")
+print(" ")
+
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {api_key}"
+}
+
+# Set request data
+data = {
+    "prompt": prompt,
+    "size": size,
+    "n": 1,
+    "response_format": response_format
+}
+
+# Send request to API
+response = requests.post(api_url, headers=headers, data=json.dumps(data))
+
+# Get image URL from response
+image_url = response.json()["data"][0]["url"]
+
+# Download image from URL and save to file
+image_data = requests.get(image_url).content
+with open("output.png", "wb") as f:
+    f.write(image_data)
+
+print(f"URL: {image_url}")
+print(" ")
+print("Saved locally as output.png!")
